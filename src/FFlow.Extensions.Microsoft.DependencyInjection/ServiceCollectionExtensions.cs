aa280c7e98434f12c28b66ae3d5875d7a036720d
Thiago Vasconcelos <thiagomvas@gmail.com>
2025-06-09 11:55:32 -0300

    docs: Add XML Doc comments to AddFlow

--- src/FFlow.Extensions.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/FFlow.Extensions.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,40 +1,50 @@
 using System.Reflection;
-using FFlow.Core;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace FFlow.Extensions.Microsoft.DependencyInjection;
-
-public static class ServiceCollectionExtensions
-{
-    public static IServiceCollection AddFFlow(this IServiceCollection services, params Assembly[] assemblies)
-    {
-        if (services == null) throw new ArgumentNullException(nameof(services));
-
-        foreach (var assembly in assemblies)
-        {
-            var stepTypes = assembly.GetTypes()
-                .Where(t => typeof(IFlowStep).IsAssignableFrom(t) && !t.IsAbstract && t.IsClass);
-
-            foreach (var stepType in stepTypes)
-            {
-                services.AddTransient(stepType);
-            }
-
-            var workflowTypes = assembly.GetTypes()
-                .Where(t => typeof(IWorkflowDefinition).IsAssignableFrom(t) && !t.IsAbstract && t.IsClass);
-            foreach (var workflowType in workflowTypes)
-            {
-                services.AddTransient(workflowType);
-                services.AddTransient(provider =>
-                {
-                    var workflowDefinition = (IWorkflowDefinition)provider.GetRequiredService(workflowType);
-                    return workflowDefinition.Build();
-                });
-            }
-        }
-
-        return services;
-    }
-
+    using FFlow.Core;
+    using Microsoft.Extensions.DependencyInjection;
     
-}
+    namespace FFlow.Extensions.Microsoft.DependencyInjection;
+    
+    /// <summary>
+    /// Provides extension methods for registering FFlow components in the dependency injection container.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Registers FFlow steps and workflow definitions from the specified assemblies into the service collection.
+        /// </summary>
+        /// <param name="services">The service collection to add the registrations to.</param>
+        /// <param name="assemblies">The assemblies to scan for FFlow steps and workflow definitions.</param>
+        /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if the <paramref name="services"/> parameter is null.</exception>
+        public static IServiceCollection AddFFlow(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+    
+            foreach (var assembly in assemblies)
+            {
+                // Register all types implementing IFlowStep
+                var stepTypes = assembly.GetTypes()
+                    .Where(t => typeof(IFlowStep).IsAssignableFrom(t) && !t.IsAbstract && t.IsClass);
+    
+                foreach (var stepType in stepTypes)
+                {
+                    services.AddTransient(stepType);
+                }
+    
+                // Register all types implementing IWorkflowDefinition
+                var workflowTypes = assembly.GetTypes()
+                    .Where(t => typeof(IWorkflowDefinition).IsAssignableFrom(t) && !t.IsAbstract && t.IsClass);
+                foreach (var workflowType in workflowTypes)
+                {
+                    services.AddTransient(workflowType);
+                    services.AddTransient(provider =>
+                    {
+                        var workflowDefinition = (IWorkflowDefinition)provider.GetRequiredService(workflowType);
+                        return workflowDefinition.Build();
+                    });
+                }
+            }
+    
+            return services;
+        }
+    }
