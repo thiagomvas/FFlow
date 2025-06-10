420e8596eddbf4e703a59657c335e8dc89bcc8fb
Thiago Vasconcelos <thiagomvas@gmail.com>
2025-06-10 12:34:22 -0300

    chore: Remove old configuration implementation

--- src/FFlow.Steps.DotNet/FlowContextExtensions.cs
+++ src/FFlow.Steps.DotNet/FlowContextExtensions.cs
@@ -4,15 +4,6 @@ namespace FFlow.Steps.DotNet;
 
 public static class FlowContextExtensions
 {
-    public static void SetDotnetConfiguration(this IFlowContext context, DotnetFlowConfiguration configuration)
-    {
-        if (context == null) throw new ArgumentNullException(nameof(context));
-        if (configuration == null) throw new ArgumentNullException(nameof(configuration));
-
-        context.Set($"{Internals.BaseNamespace}.Configuration", configuration);
-        
-    }
-    
     public static void SetDotnetBuildConfig(this IFlowContext context, DotnetBuildConfiguration config) 
     {
         if (context == null) throw new ArgumentNullException(nameof(context));
