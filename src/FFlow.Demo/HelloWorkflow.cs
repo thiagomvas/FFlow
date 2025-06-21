eb5f7edf4cee68cf15e184472d1c102311393d9a
Thiago Vasconcelos <thiagomvas@gmail.com>
2025-06-20 21:58:40 -0300

    feat: Add new base WorkflowDefinition class to introduce default behaviours for workflow definitions

--- src/FFlow.Demo/HelloWorkflow.cs
+++ src/FFlow.Demo/HelloWorkflow.cs
@@ -1,13 +1,32 @@
 using FFlow.Core;
+using FFlow.Extensions;
+using FFlow.Observability.Listeners;
 
 namespace FFlow.Demo;
 
-public class HelloWorkflow : IWorkflowDefinition
+public class HelloWorkflow : WorkflowDefinition
 {
-    public IWorkflow Build()
+    public HelloWorkflow()
     {
-        return new FFlowBuilder()
+        MetadataStore.SetName("Hello Workflow")
+            .SetDescription("A simple workflow that greets the world and then says goodbye.");
+    }
+
+    public override void OnConfigure(IWorkflowBuilder builder)
+    {
+        builder
             .StartWith<HelloStep>()
-            .Build();
+            .Input<HelloStep, string>(step => step.Name, "World")
+            .Delay(1000)
+            .Then<GoodByeStep>()
+            .Input<GoodByeStep, string>(step => step.Name, "World");
+    }
+
+    public override Action<WorkflowOptions> OnConfigureOptions()
+    {
+        return options =>
+        {
+            options.WithEventListener(new ConsoleFlowEventListener());
+        };
     }
 }
