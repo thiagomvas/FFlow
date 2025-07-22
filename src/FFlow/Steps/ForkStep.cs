fd11a3a00c1c673f20b76b4a613f8c889ad2bb3d
Thiago Vasconcelos <thiagomvas@gmail.com>
2025-07-22 11:03:57 -0300

    feat: Add custom graph for Fork step

--- src/FFlow/Steps/ForkStep.cs
+++ src/FFlow/Steps/ForkStep.cs
@@ -1,12 +1,13 @@
 using FFlow.Core;
 using FFlow.Extensions;
+using FFlow.Visualization;
 
 namespace FFlow;
 
 [StepName("Fork")]
 [StepTags("built-in")]
 [SilentStep]
-public class ForkStep : IFlowStep
+public class ForkStep : IFlowStep, IDescribableStep
 {
     private readonly Func<IWorkflowBuilder>[] _forks;
     private readonly ForkStrategy _forkStrategy;
@@ -34,7 +35,6 @@ public class ForkStep : IFlowStep
         switch (_forkStrategy)
         {
             case ForkStrategy.FireAndForget:
-                // fire-and-forget but still tracked
                 _ = Task.WhenAll(tasks);
                 break;
             case ForkStrategy.WaitForAll:
@@ -42,4 +42,58 @@ public class ForkStep : IFlowStep
                 break;
         }
     }
+
+    public WorkflowGraph Describe(string? rootId = null)
+    {
+        var graph = new WorkflowGraph();
+        var metadata = StepMetadataRegistry.Instance.Value.GetMetadata(this.GetType());
+
+        rootId ??= metadata.Id;
+        var rootNode = new WorkflowNode(rootId, metadata.Name);
+        graph.Nodes.Add(rootNode);
+
+        var forkNode = new WorkflowNode($"{rootId}_fork", $"Fork - {_forkStrategy}");
+        graph.Nodes.Add(forkNode);
+        graph.Edges.Add(new WorkflowEdge(rootNode.Id, forkNode.Id));
+
+        if (_forkStrategy == ForkStrategy.WaitForAll)
+        {
+            var mergeNode = new WorkflowNode($"{rootId}_merge", "Join");
+            graph.Nodes.Add(mergeNode);
+
+            for (int idx = 0; idx < _forks.Length; idx++)
+            {
+                var builder = _forks[idx]();
+                var subgraph = builder.Describe();
+                var (entryId, exitIds) = graph.Merge(subgraph, $"{rootId}_branch{idx}");
+
+                graph.Edges.Add(new WorkflowEdge(forkNode.Id, entryId, $"Branch {idx + 1}"));
+
+                foreach (var exitId in exitIds)
+                {
+                    graph.Edges.Add(new WorkflowEdge(exitId, mergeNode.Id));
+                }
+            }
+
+            return graph;
+        }
+        else 
+        {
+            for (int idx = 0; idx < _forks.Length; idx++)
+            {
+                var builder = _forks[idx]();
+                var subgraph = builder.Describe();
+                var (entryId, exitIds) = graph.Merge(subgraph, $"{rootId}_branch{idx}");
+
+                graph.Edges.Add(new WorkflowEdge(forkNode.Id, entryId, $"Branch {idx + 1}"));
+            }
+            var virtualExitNode = new WorkflowNode($"{rootId}_mainthread", "Continue");
+            graph.Nodes.Add(virtualExitNode);
+
+            graph.Edges.Add(new WorkflowEdge(forkNode.Id, virtualExitNode.Id, "Main Thread"));
+            return graph;
+        }
+    }
+
+
 }
