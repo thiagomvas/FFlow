da49b3ef2e480efe1b71307f23871f1b888ff091
Thiago Vasconcelos <thiagomvas@gmail.com>
2025-07-22 11:15:32 -0300

    feat: Add custom graph for foreach step

--- src/FFlow/Steps/ForEachStep.cs
+++ src/FFlow/Steps/ForEachStep.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using FFlow.Core;
+using FFlow.Visualization;
 
 namespace FFlow;
 
@@ -14,7 +15,7 @@ public class ForEachStep : ForEachStep<object>
     }
 }
 
-public class ForEachStep<T> : IFlowStep
+public class ForEachStep<T> : IFlowStep, IDescribableStep
 {
     private readonly Func<IFlowContext, IEnumerable<T>> _itemsSelector;
     private readonly IFlowStep _itemAction;
@@ -41,5 +42,52 @@ public class ForEachStep<T> : IFlowStep
             context.SetInputFor(_itemAction, item);
             await _itemAction.RunAsync(context, cancellationToken);
         }
+    }
+
+    public WorkflowGraph Describe(string? rootId = null)
+    {
+        var graph = new WorkflowGraph();
+        var metadata = StepMetadataRegistry.Instance.Value.GetMetadata(this.GetType());
+
+        var genericType = typeof(T).Name;
+        var displayLabel = $"ForEach<{genericType}>";
+
+        rootId ??= metadata.Id;
+
+        var rootNode = new WorkflowNode(rootId, displayLabel);
+        graph.Nodes.Add(rootNode);
+
+        var iteratorNode = rootNode;
+
+        var doneNode = new WorkflowNode($"{rootId}_done", "Done");
+        graph.Nodes.Add(doneNode);
+
+        if (_itemAction is IDescribableStep describable)
+        {
+            var subgraph = describable.Describe();
+            var (entryId, exitIds) = graph.Merge(subgraph, $"{rootId}_item");
+
+            graph.Edges.Add(new WorkflowEdge(iteratorNode.Id, entryId, "Each item"));
+
+            foreach (var exitId in exitIds)
+            {
+                // Loop back to iterator
+                graph.Edges.Add(new WorkflowEdge(exitId, iteratorNode.Id, "Next"));
+            }
+        }
+        else
+        {
+            var actionNodeId = $"{rootId}_item_action";
+            graph.Nodes.Add(new WorkflowNode(actionNodeId, _itemAction.GetType().Name));
+            graph.Edges.Add(new WorkflowEdge(iteratorNode.Id, actionNodeId, "Each item"));
+
+            // Loop back
+            graph.Edges.Add(new WorkflowEdge(actionNodeId, iteratorNode.Id, "Next"));
+        }
+
+        // Exit path from iterator to Done (breaks the loop)
+        graph.Edges.Add(new WorkflowEdge(iteratorNode.Id, doneNode.Id, "Done"));
+
+        return graph;
     }
 }
