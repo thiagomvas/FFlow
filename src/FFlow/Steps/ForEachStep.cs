60f5287af090b0d42afbf38b1ec3c13fdd7d5c7c
Thiago Vasconcelos <thiagomvas@gmail.com>
2025-08-02 22:10:56 -0300

    feat: Add new ForEachStep implementation

--- src/FFlow/Steps/ForEachStep.cs
+++ src/FFlow/Steps/ForEachStep.cs
@@ -1,93 +1,78 @@
 using System.Collections;
 using FFlow.Core;
-using FFlow.Visualization;
 
 namespace FFlow;
 
-[StepName("For Each")]
-[StepTags("built-in")]
-[SilentStep]
-public class ForEachStep : ForEachStep<object>
+public class ForEachStep<TItem, TStepIterator> : FlowStep
+    where TStepIterator : IFlowStep
 {
-    public ForEachStep(Func<IFlowContext, IEnumerable> itemsSelector, IFlowStep itemAction)
-        : base(context => itemsSelector(context).Cast<object>(), itemAction)
+    private readonly Func<IFlowContext, IEnumerable<TItem>> _itemsSelector;
+    private readonly Action<TItem>? _executor;
+    private readonly Action<TItem, TStepIterator>? _configurator;
+    private readonly Func<TStepIterator> _stepFactory;
+
+    public ForEachStep(Func<IFlowContext, IEnumerable<TItem>> itemsSelector, Action<TItem> executor)
+    {
+        _itemsSelector = itemsSelector ?? throw new ArgumentNullException(nameof(itemsSelector));
+        _executor = executor ?? throw new ArgumentNullException(nameof(executor));
+    }
+
+    public ForEachStep(Func<IFlowContext, IEnumerable<TItem>> itemsSelector, Func<TStepIterator> factory,
+        Action<TItem, TStepIterator> configurator)
+    {
+        _itemsSelector = itemsSelector ?? throw new ArgumentNullException(nameof(itemsSelector));
+        _configurator = configurator ?? throw new ArgumentNullException(nameof(configurator));
+        _stepFactory = factory ?? throw new ArgumentNullException(nameof(factory));
+    }
+
+    protected override async Task ExecuteAsync(IFlowContext context, CancellationToken cancellationToken)
+    {
+        var items = _itemsSelector(context);
+        if (items is null || !items.Any()) return;
+
+        foreach (var item in items)
+        {
+            if (_executor != null)
+            {
+                _executor(item);
+            }
+            else if (_configurator != null)
+            {
+                var step = _stepFactory();
+                _configurator(item, step);
+                await step.RunAsync(context);
+            }
+            else
+            {
+                throw new InvalidOperationException("Either executor or configurator must be provided.");
+            }
+        }
+    }
+}
+
+public class ForEachStep<TItem> : ForEachStep<TItem, FlowStep>
+{
+    public ForEachStep(Func<IFlowContext, IEnumerable<TItem>> itemsSelector, Action<TItem> executor)
+        : base(itemsSelector, executor)
+    {
+    }
+
+    public ForEachStep(Func<IFlowContext, IEnumerable<TItem>> itemsSelector, Action<TItem, FlowStep> configurator)
+        : base(itemsSelector, null, configurator)
     {
     }
 }
 
-public class ForEachStep<T> : IFlowStep, IDescribableStep
+
+public class ForEachStep : ForEachStep<object, FlowStep>
 {
-    private readonly Func<IFlowContext, IEnumerable<T>> _itemsSelector;
-    private readonly IFlowStep _itemAction;
-    
-    public ForEachStep(Func<IFlowContext, IEnumerable<T>> itemsSelector, IFlowStep itemAction)
+    public ForEachStep(Func<IFlowContext, IEnumerable<object>> itemsSelector, Action<object> executor)
+        : base(context => itemsSelector(context), executor)
     {
-        _itemsSelector = itemsSelector ?? throw new ArgumentNullException(nameof(itemsSelector));
-        _itemAction = itemAction ?? throw new ArgumentNullException(nameof(itemAction));
     }
 
-    public async Task RunAsync(IFlowContext context, CancellationToken cancellationToken = default)
+    public ForEachStep(Func<IFlowContext, IEnumerable<object>> itemsSelector, Action<object, FlowStep> configurator)
+        : base(context => itemsSelector(context), null, configurator)
     {
-        cancellationToken.ThrowIfCancellationRequested();
-
-        if (context == null) throw new ArgumentNullException(nameof(context));
-        if (_itemsSelector == null) throw new InvalidOperationException("Items selector must be set.");
-        if (_itemAction == null) throw new InvalidOperationException("Item action must be set.");
-
-        var items = _itemsSelector(context);
-        if (items == null) return;
-
-        foreach (var item in items)
-        {
-            context.SetInputFor(_itemAction, item);
-            await _itemAction.RunAsync(context, cancellationToken);
-        }
-    }
-
-    public WorkflowGraph Describe(string? rootId = null)
-    {
-        var graph = new WorkflowGraph();
-        var metadata = StepMetadataRegistry.Instance.Value.GetMetadata(this.GetType());
-
-        var genericType = typeof(T).Name;
-        var displayLabel = $"ForEach<{genericType}>";
-
-        rootId ??= metadata.Id;
-
-        var rootNode = new WorkflowNode(rootId, displayLabel);
-        graph.Nodes.Add(rootNode);
-
-        var iteratorNode = rootNode;
-
-        var doneNode = new WorkflowNode($"{rootId}_done", "Done");
-        graph.Nodes.Add(doneNode);
-
-        if (_itemAction is IDescribableStep describable)
-        {
-            var subgraph = describable.Describe();
-            var (entryId, exitIds) = graph.Merge(subgraph, $"{rootId}_item");
-
-            graph.Edges.Add(new WorkflowEdge(iteratorNode.Id, entryId, "Each item"));
-
-            foreach (var exitId in exitIds)
-            {
-                // Loop back to iterator
-                graph.Edges.Add(new WorkflowEdge(exitId, iteratorNode.Id, "Next"));
-            }
-        }
-        else
-        {
-            var actionNodeId = $"{rootId}_item_action";
-            graph.Nodes.Add(new WorkflowNode(actionNodeId, _itemAction.GetType().Name));
-            graph.Edges.Add(new WorkflowEdge(iteratorNode.Id, actionNodeId, "Each item"));
-
-            // Loop back
-            graph.Edges.Add(new WorkflowEdge(actionNodeId, iteratorNode.Id, "Next"));
-        }
-
-        graph.ContinueFromId = rootId;
-        graph.ExitEdgeLabel = "Done";
-
-        return graph;
     }
 }
