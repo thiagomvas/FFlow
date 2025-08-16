1a4d1ceb74d536de9ad65eff0bed4c277368de84
Thiago Vasconcelos <thiagomvas@gmail.com>
2025-08-16 11:26:41 -0300

    feat(git): Add 'git init' step

---
+++ FFlow.Steps.Git/GitInitCommand.cs
@@ -0,0 +1,28 @@
+using FFlow.Core;
+
+namespace FFlow.Steps.Git;
+
+public class GitInitCommand : GitStepBase
+{
+    public string DirectoryPath { get; set; } = string.Empty;
+
+    public bool Bare { get; set; } = false;  
+    public bool Quiet { get; set; } = false; 
+
+    protected override Task ExecuteAsync(IFlowContext context, CancellationToken cancellationToken)
+    {
+        string path = string.IsNullOrWhiteSpace(DirectoryPath)
+            ? Directory.GetCurrentDirectory()
+            : DirectoryPath;
+
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var args = new List<string>();
+        if (Bare) args.Add("--bare");
+        if (Quiet) args.Add("-q");
+
+        args.AddRange(AdditionalArgs); 
+
+        return GitProvider.GitInitializeAsync(path, cancellationToken, args.ToArray());
+    }
+}
