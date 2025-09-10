47d454c29aa989a56837f9658db6193163ccdc19
Thiago Vasconcelos <thiagomvas@gmail.com>
2025-09-10 20:11:33 -0300

    chore: Remove non-scriptable packages from cli initializer

--- src/FFlow.Cli/Internals.cs
+++ src/FFlow.Cli/Internals.cs
@@ -9,8 +9,6 @@ public static class Internals
         { "SFTP", "FFlow.Steps.SFTP" },
         { "File IO", "FFlow.Steps.FileIO" },
         { "HTTP Requests", "FFlow.Steps.Http" },
-        { "Scheduling", "FFlow.Scheduling" },
-        { "Metrics & Observability", "FFlow.Observability" },
     };
     
     public const string DockerImage = "mcr.microsoft.com/dotnet/sdk:10.0-preview";
